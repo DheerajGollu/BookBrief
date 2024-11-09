f98fb1e3c66abe0d8ce83bda9f82d951c9fdb7bf
KevVvTheDev <kevintran11082002@gmail.com>
2024-11-09 11:52:39 -0500

    can finally send data from front end to back end

--- client-side/src/App.jsx
+++ client-side/src/App.jsx
@@ -1,21 +1,38 @@
-import { useEffect } from 'react'
+import {  useState } from 'react'
 import axios from 'axios' //to send requests to server
 import '../styles/app.css'
 function App() {
+  const [bookName, setBookName] = useState('');
+  const handleInput = (event) =>{
+    setBookName(event.target.value);
+  } //handle input change so value can match
   const fetchData = async () => {
-    const response = await axios.get('http://localhost:8080/'); //fetch data from server
-    console.log(response.data); //log data for testing purpose
-  }
-  useEffect(() => { 
-    fetchData();  
-  }, []); //run only once
+    try {
+      // Send a POST request with bookName in the request body
+      const response = await axios.post('http://localhost:8080/book', { bookName });
+      console.log(response.data); // Log data for testing purposes
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  } 
+
 
   return (
     <div className='app'>
     <h1>Welcome to Book Brief!</h1>
-    <input type="text" />
+
+    <input type="text" 
+    name = "bookName"
+    value = {bookName}
+    onChange = {handleInput}
+    placeholder='Enter book title...'
+    />
+     {/* When the button is clicked, it calls fetchData to send bookName */}
+     <button onClick={fetchData}>Submit</button> 
+    
+    <p>{bookName}</p>
      </div>
-  )
+  );
 }
 
 export default App
