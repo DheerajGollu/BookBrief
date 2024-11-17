ae5f94aa2d64e02d092b180c04612b9faa2697df
KevVvTheDev <kevintran11082002@gmail.com>
2024-11-16 20:03:27 -0500

    refactoring the code

--- client-side/src/main.jsx
+++ client-side/src/main.jsx
@@ -2,6 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import '../styles/style.css'
+import Axios from 'axios'
+
+Axios.defaults.baseURL = "http://localhost:8080" // This is the backend URL
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
