fa7eff6188b447b4945bad1144731177c5ec338f
Your Name <you@example.com>
2024-11-16 19:24:11 -0500

    llm api working

---
+++ server/gemini_api.js
@@ -0,0 +1,12 @@
+import dotenv from "dotenv";
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+dotenv.config();
+
+const genAI = new GoogleGenerativeAI("AIzaSyBKVJXJ111S-ZVWGx0dDYEz7mCvJfWqdLg");
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+const prompt = "Provide a short summary of Red Rising book and provide a list of 5 similar books.";
+
+const result = await model.generateContent(prompt);
+console.log(result.response.text());
