aaa22d97954e53d5e19c4bc887d6d9ead9256131
Your Name <you@example.com>
2024-11-16 19:24:45 -0500

    second commit

--- server/gemini_api.js
+++ server/gemini_api.js
@@ -6,7 +6,7 @@ dotenv.config();
 const genAI = new GoogleGenerativeAI("AIzaSyBKVJXJ111S-ZVWGx0dDYEz7mCvJfWqdLg");
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
-const prompt = "Provide a short summary of Red Rising book and provide a list of 5 similar books.";
+const prompt = "Provide a short summary of 'Red Rising' book and provide a list of 5 similar books.";
 
 const result = await model.generateContent(prompt);
 console.log(result.response.text());
